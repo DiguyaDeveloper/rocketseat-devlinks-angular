cbb51aa081cbab00d38fbb3e9316ea04eb1bc482
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-08-08 16:52:51 -0300

    feat: test github actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'devlinks-angular';
+  title = "Diguya devlinks-angular"
 }
